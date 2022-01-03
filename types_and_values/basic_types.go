3b76a386c494ba564247fc4ee78b255c8aa3d117
tjysdsg <tjy1018543509@163.com>
2022-01-03 22:35:28 +0800

    Add basic type and variable declarations

---
+++ types_and_values/basic_types.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func main() {
+	var speed1 int = 100
+	var speed2 = 100
+	speed3 := 100
+
+	var (
+		fp1 float32 = 3.1415
+		fp2         = 3.1415
+	)
+
+	var s1 = "a string"
+
+	var _, _ bool
+	var b1, b2 = true, false
+
+	fmt.Println("Integer values:", speed1, speed2, speed3)
+	fmt.Println("Default int size:", unsafe.Sizeof(speed1), "bytes") // However: https://pkg.go.dev/builtin#int
+	fmt.Println("Float values:", fp1)
+	fmt.Println("Default float type:", reflect.TypeOf(fp2))
+
+	fmt.Printf("s1 = %q\n", s1)
+
+	fmt.Println("bools:", b1, b2)
+}
